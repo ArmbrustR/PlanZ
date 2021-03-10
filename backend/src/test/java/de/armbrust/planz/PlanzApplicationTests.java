81683ab55ba28b614a882b78dac15df280fee5b6
ArmbrustR <rafael.armbrust@gmail.com>
2021-03-10 21:00:26 +0100

    init backend

---
+++ backend/src/test/java/de/armbrust/planz/PlanzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.armbrust.planz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
